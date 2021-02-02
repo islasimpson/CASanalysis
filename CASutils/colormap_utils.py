e33f075a6974942e2de2c678cbaf2bae25347c61
Isla Simpson <islas@tungsten.cgd.ucar.edu>
2021-02-02 15:28:53 -0700

    adding custom color maps

---
+++ CASutils/colormap_utils.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap ## used to create custom colormaps
+import matplotlib.colors as mcolors
+import numpy as np
+
+def blue2red_cmap(n):
+    """ combine two existing color maps to create a diverging color map with white in the middle
+    n = the number of contour intervals
+    """
+
+    if (int(n/2) == n/2):
+        # even number of contours
+        nwhite=1
+        nneg=n/2
+        npos=n/2
+    else:
+        nwhite=2
+        nneg = (n-1)/2
+        npos = (n-1)/2
+
+    colors1 = plt.cm.Blues_r(np.linspace(0,1, int(nneg)))
+    colors2 = plt.cm.YlOrRd(np.linspace(0,1, int(npos)))
+    colorsw = np.ones((nwhite,4))
+
+    colors = np.vstack((colors1, colorsw, colors2))
+    mymap = mcolors.LinearSegmentedColormap.from_list('my_colormap', colors)
+
+    return mymap 
