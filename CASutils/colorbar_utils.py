58b4bad49291c8e753ba84dcc840dac728e7fdc3
Isla Simpson <islas@tungsten.cgd.ucar.edu>
2021-02-02 18:32:31 -0700

    adding colorbar_utils.py

---
+++ CASutils/colorbar_utils.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from CASutils import colormap_utils as mycolors
+import numpy as np
+
+def plotcolorbar(fig, ci, cmin, cmax, titlestr, x1, x2, y1, y2, 
+   cmap='blue2red', orient='horizontal', posneg='both', ticks=None, fsize=14):
+    """plot a color bar
+       Input:
+           fig = the figure identified
+           ci = the contour interval for the color map
+           cmin = the minimum extent of the contour range
+           cmax = the maximum extent of the contour range
+           titlestr = the label for the color bar
+           x1 = the location of the left edge of the color bar
+           x2 = the location of the right edge of the color bar
+           y1 = the location of the bottom edge of the color bar
+           y2 = the location of the top edge of the color bar
+           cmap = the color map to be used (only set up for blue2red at the moment)
+           orient = the orientation (horizontal or vertical)
+           posneg = if "both", both positive and negative sides are plotted
+                    if "pos", only the positive side is plotted
+                    if "net", only the negative side is plotted
+           ticks = user specified ticklabels
+           fsize = user specified font size
+    """
+
+    # set up contour levels and color map
+    nlevs = (cmax-cmin)/ci + 1
+    clevs = np.arange(cmin, cmax+ci, ci)
+
+    if (cmap == "blue2red"):
+        mymap = mycolors.blue2red_cmap(nlevs)
+
+    clevplot=clevs
+    if (posneg == "pos"):
+        clevplot = clevs[clevs >= 0]
+    if (posneg == "neg"):
+        clevplot = clevs[clevs <= 0]
+
+
+    ax = fig.add_axes([x1, y1, x2-x1, y2-y1])
+    norm = mpl.colors.Normalize(vmin=cmin, vmax=cmax)
+    
+    if (ticks):
+        clb = mpl.colorbar.ColorbarBase(ax, cmap=mymap,
+           orientation=orient, norm=norm, values=clevplot, ticks=ticks)
+    else:
+        clb = mpl.colorbar.ColorbarBase(ax, cmap=mymap, 
+           orientation=orient, norm=norm, values=clevplot)
+
+    clb.ax.tick_params(labelsize=fsize)
+    clb.set_label(titlestr, fontsize=fsize+2)
+
+    return ax
