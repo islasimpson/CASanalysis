9dae0613f8619df56f1089b37932da11590cbf7f
Isla Simpson <islas@tungsten.cgd.ucar.edu>
2021-02-04 13:19:30 -0700

    modifying seasonal cycle filtering to deal with 1d arrays

--- CASutils/filter_utils.py
+++ CASutils/filter_utils.py
@@ -13,7 +13,7 @@ def calc_season_nharm(darray, nharms, dimtime=0):
 
     """
     # get the dimensions of the input array
-    #dims = darray.dims
+    dims = darray.dims
 
 
     # convert to a numpy array
@@ -26,26 +26,39 @@ def calc_season_nharm(darray, nharms, dimtime=0):
     ntime = darray_np.shape[0]
     shapein = darray_np.shape
     # collapse dimensions 1 to n
-    darray_np = darray_np.reshape( darray_np.shape[0], np.prod(darray_np.shape[1::]))
+    if (len(dims) > 1):
+        darray_np = darray_np.reshape( darray_np.shape[0], np.prod(darray_np.shape[1::]))
 
     # check for NaNs
-    numnans = [np.count_nonzero(np.isnan(darray_np[:,i])) for i in range(0,darray_np.shape[1],1)]
-    nanels = np.where(np.array(numnans) > 0)
+    if (len(dims) > 1):
+        numnans = [np.count_nonzero(np.isnan(darray_np[:,i])) for i in range(0,darray_np.shape[1],1)]
+        nanels = np.where(np.array(numnans) > 0)
 
-    # if nan's exist, del with them by linear interpolation
-    if np.any(nanels):
-        print("you got nans")
-        i = np.arange(ntime)
-        for j in np.array(nanels[0]):
-            # check if there are values that are not Nan's
-            if (~np.isnan(darray_np[:,j]).any()):
-                mask = np.isfinite(darray_np[:,j])
-                darray_np[:,j] = np.interp(i,i[mask],darray_np[mask,j])
+        # if nan's exist, del with them by linear interpolation
+        if np.any(nanels):
+            print("you got nans")
+            i = np.arange(ntime)
+            for j in np.array(nanels[0]):
+                # check if there are values that are not Nan's
+                if (~np.isnan(darray_np[:,j]).any()):
+                    mask = np.isfinite(darray_np[:,j])
+                    darray_np[:,j] = np.interp(i,i[mask],darray_np[mask,j])
+
+    else: # doing this differently for a 1D array
+        nanels = np.count_nonzero(np.isnan(darray_np))
+        if (nanels > 0):
+            mask = np.isfinite(darray_np[:])
+            darray_np[:] = np.interp(i,i[mask],darray_np[mask])
+
 
     tempft = fft(darray_np, axis=0)
     tempft2 = np.zeros_like(tempft)
-    tempft2[0:nharms,:] = tempft[0:nharms,:]
-    tempft2[ntime-nharms+1:ntime+1,:] = tempft[ntime-nharms+1:ntime+1,:]
+#    tempft2[0:nharms,:] = tempft[0:nharms,:]
+#    tempft2[ntime-nharms+1:ntime+1,:] = tempft[ntime-nharms+1:ntime+1,:]
+    tempft2[0:nharms] = tempft[0:nharms]
+    tempft2[ntime-nharms+1:ntime+1] = tempft[ntime-nharms+1:ntime+1]
+
+
 
     darray_filtered = np.real(ifft(tempft2, axis=0))
 
