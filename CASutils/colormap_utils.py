bdbf1f9e425362391765b21f65b34914a947ee97
Isla Simpson <islas@tungsten.cgd.ucar.edu>
2021-02-10 11:14:09 -0700

    adding precip color map

--- CASutils/colormap_utils.py
+++ CASutils/colormap_utils.py
@@ -26,3 +26,29 @@ def blue2red_cmap(n):
     mymap = mcolors.LinearSegmentedColormap.from_list('my_colormap', colors)
 
     return mymap 
+
+def precip_cmap(n):
+    """ combine two existing color maps to create a diverging color map with white in the middle.
+    browns for negative, blues for positive
+    n = the number of contour intervals
+    """
+    if (int(n/2) == n/2):
+        # even number of contours
+        nwhite=1
+        nneg=n/2
+        npos=n/2
+    else:
+        nwhite=2
+        nneg = (n-1)/2
+        npos = (n-1)/2
+
+    colors1 = plt.cm.YlOrBr_r(np.linspace(0,1, int(nneg)))
+    colors2 = plt.cm.GnBu(np.linspace(0,1, int(npos)))
+    colorsw = np.ones((nwhite,4))
+
+    colors = np.vstack((colors1, colorsw, colors2))
+    mymap = mcolors.LinearSegmentedColormap.from_list('my_colormap', colors)
+
+    return mymap
+
+
