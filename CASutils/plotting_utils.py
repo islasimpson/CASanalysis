0b2483709c6ad6dd156f17afc9827cc963249102
Isla Simpson <islas@tungsten.cgd.ucar.edu>
2021-03-17 11:26:37 -0600

    debugging

--- CASutils/plotting_utils.py
+++ CASutils/plotting_utils.py
@@ -6,4 +6,5 @@ def plotbox(fig, x1,x2,y1,y2, color='red'):
     ax = fig.add_axes([x1, y1, x2-x1, y2-y1])
     box = Rectangle((0.,0.),1,1,facecolor=color)
     ax.axis('off')
+    ax.add_patch(box)
     return ax
