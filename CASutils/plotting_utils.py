d9a5489023425724b269b6ca78af8da6c3fa1aee
Isla Simpson <islas@tungsten.cgd.ucar.edu>
2021-03-17 11:20:56 -0600

    debug

--- CASutils/plotting_utils.py
+++ CASutils/plotting_utils.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 
-def plotbox(x1,x2,y1,y2, color='red'):
+def plotbox(fig, x1,x2,y1,y2, color='red'):
     """plot a colored box"""
     ax = fig.add_axes([x1, y1, x2-x1, y2-y1])
-    box = Rectangle((0.,0.),1,1 facecolor=color)
+    box = Rectangle((0.,0.),1,1,facecolor=color)
     ax.axis('off')
     return ax
