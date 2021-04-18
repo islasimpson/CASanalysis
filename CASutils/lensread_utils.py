03f21dfa10198f532dff7d309a037a5ab138db33
Isla Simpson <islas@tungsten.cgd.ucar.edu>
2021-04-18 09:26:23 -0600

    updating lensread

--- CASutils/lensread_utils.py
+++ CASutils/lensread_utils.py
@@ -24,6 +24,45 @@ def lens2memnamegen(nmems):
             memstr2=str(imem+1).zfill(3)
             memstr.append(memstr1+'.'+memstr2)
         
+        if ((imem >= 10) and (imem < 30)):
+            memstr1=str(1231)
+            memstr2=str(imem-10+1).zfill(3)
+            memstr.append(memstr1+'.'+memstr2)
+
+        if ((imem >= 30) and (imem < 50)):
+            memstr1=str(1251)
+            memstr2=str(imem-30+1).zfill(3)
+            memstr.append(memstr1+'.'+memstr2)
+
+        if ((imem >= 50) and (imem < 70)):
+            memstr1=str(1281)
+            memstr2=str(imem-50+1).zfill(3)
+            memstr.append(memstr1+'.'+memstr2)
+
+        if ((imem >= 70) and (imem < 90)):
+            memstr1=str(1301)
+            memstr2=str(imem-70+1).zfill(3)
+            memstr.append(memstr1+'.'+memstr2)
+
+ 
+    return memstr
+
+def lens2memnamegen_first50(nmems):
+    """Generate the member names for LENS2 simulations
+    Input: 
+      nmems = number of members
+    Output:
+      memstr(nmems) = an array containing nmems strings corresponding to the member names
+    """
+
+    memstr=[]
+    for imem in range(0,nmems,1):
+   
+        if (imem < 10):
+            memstr1=str(1000+imem*20+1)
+            memstr2=str(imem+1).zfill(3)
+            memstr.append(memstr1+'.'+memstr2)
+        
         if ((imem >= 10) and (imem < 20)):
             memstr1=str(1231)
             memstr2=str(imem-10+1).zfill(3)
@@ -46,6 +85,10 @@ def lens2memnamegen(nmems):
 
  
     return memstr
+
+
+
+
 
 def lens2groups():
     """Generate the member names for LENS 2 simulations an output them in groups
