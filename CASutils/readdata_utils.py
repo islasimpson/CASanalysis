3786e7dc16e08eef861db133736923251200bcaf
Isla Simpson <islas@tungsten.cgd.ucar.edu>
2024-01-19 17:03:10 -0700

    adding in a routine for replacing daily times with the average of time_bnds including a fix for the leap years

--- CASutils/readdata_utils.py
+++ CASutils/readdata_utils.py
@@ -30,6 +30,27 @@ def fixcesmtime(dat,timebndsvar='time_bnds'):
 
     return dat
 
+def fixcesmtime_daily(dat, timebndsvar='time_bnds'):
+    """ Fix the CESM timestamp using the average of time_bnds
+        Add in a fix of the times for the leap years """
+
+    try:
+        timebndavg = np.array(dat.isel(M=0)[timebndsvar],
+                  dtype='datetime64[s]').view('i8').mean(axis=1).astype('datetime64[s]')
+    except:
+        timebndavg = np.array(dat[timebndsvar],
+                  dtype='datetime64[s]').view('i8').mean(axis=1).astype('datetime64[s]')
+
+    dates = pd.DatetimeIndex(timebndavg)
+    lyindices = np.argwhere( (dates.month == 2) & (dates.day == 29) )
+    if (len(lyindices) > 0):
+        for i in lyindices:
+            timebndavg[i] = str(dates.year[i].item())+"-02-28T12:00:00"
+
+    dat['time'] = timebndavg
+
+    return dat
+
 
 def read_cesm_h0(fpath, datestart, dateend, var):
     """Read in a variable from CESM history files.  Adapted for 
