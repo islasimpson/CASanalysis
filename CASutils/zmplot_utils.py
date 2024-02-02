5a73a3ca2824cae7370d8a87778450ea1fab4dda
Isla Simpson <islas@tungsten.cgd.ucar.edu>
2024-02-02 07:00:22 -0700

    adding 100hpa to 0.1hpa plot

--- CASutils/zmplot_utils.py
+++ CASutils/zmplot_utils.py
@@ -100,6 +100,44 @@ def plotlatlogpre_to1(fig, data, lat, pre, ci, cmin, cmax, titlestr, x1=0.1, x2=
     return ax
 
 
+def plotlatlogpre_100to0p1(fig, data, lat, pre, ci, cmin, cmax, titlestr, x1=0.1, x2=0.9, y1=0.1, y2=0.9, ylabel=True, yticklabels=True, fsize=11, cmap='blue2red'):
+    """
+    Plot a pressure versus latitude contour plot from 100hPa up to 0.1hPa.
+    """
+
+    # set up contour levels and color map
+    nlevs = (cmax-cmin)/ci + 1
+    clevs = np.arange(cmin, cmax+ci, ci)
+
+    if (cmap == "blue2red"):
+        mymap = mycolors.blue2red_cmap(nlevs)
+
+    if (cmap == "precip"):
+        mymap = mycolors.precip_cmap(nlevs)
+
+    if (cmap == "precip_nowhite"):
+        mymap = mycolors.precip_cmap_nowhite(nlevs)
+
+    plt.rcParams['font.size'] = fsize 
+
+    ax = fig.add_axes([x1, y1, x2-x1, y2-y1])
+
+    ax.contourf(lat,-1.*np.log10(pre), data, levels=clevs, cmap=mymap, extend='max')
+    ax.contour(lat,-1.*np.log10(pre), data, levels=clevs[ clevs != 0], colors='black', linewidths=0.5)
+    ax.set_ylim(-np.log10(100.),-np.log10(0.1))
+    ax.set_yticks([-np.log10(100),-np.log10(10),-np.log10(1),-np.log10(0.1)])
+
+    if (yticklabels):
+        ax.set_yticklabels(['100','10','1','0.1'])
+    else:
+        ax.set_yticklabels([' ',' ',' ',' '])
+    if (ylabel):
+        ax.set_ylabel('Pressure (hPa)', labelpad=-4)
+    ax.set_title(titlestr, fontsize=fsize+2)
+    ax.set_xlabel('Latitude $^{\circ}$N')
+
+    return ax
+
 
 
 def plotlatlogpre_to0p1(fig, data, lat, pre, ci, cmin, cmax, titlestr, x1=0.1, x2=0.9, y1=0.1, y2=0.9, ylabel=True, yticklabels=True, fsize=11, cmap='blue2red'):
