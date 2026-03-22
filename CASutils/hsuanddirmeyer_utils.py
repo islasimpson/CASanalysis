19bf1205240be2a8bd91a8a315fdb53f5f7c4092
Isla Simpson <islas@tungsten.cgd.ucar.edu>
2026-03-22 12:50:10 -0600

    adding in transformatin of curve_type from integer to string to allow vectorizing

--- CASutils/hsuanddirmeyer_utils.py
+++ CASutils/hsuanddirmeyer_utils.py
@@ -71,6 +71,26 @@ def get_fit_for_given_sm(xvals, fitdat):
 
     yvals = xvals*0
 
+    # for vectorizing, having an integer value that corresponds to the curve type and 
+    # converting if the curve_type input is integer
+    # Mapping:
+    # 001 = 1
+    # 010 = 2
+    # 011 = 3
+    # 110 = 4
+    # 111 = 5
+    if fitdat.curve_type.dtype == 'int64':
+        if fitdat.curve_type == 1:
+            fitdat['curve_type'] = '001'
+        if fitdat.curve_type == 2:
+            fitdat['curve_type'] = '010'
+        if fitdat.curve_type == 3:
+            fitdat['curve_type'] = '011'
+        if fitdat.curve_type == 4:
+            fitdat['curve_type'] = '110'
+        if fitdat.curve_type == 5:
+            fitdat['curve_type'] = '111'
+
     if (fitdat.curve_type == '111'):
 
         x0 = np.array(fitdat.x0)
